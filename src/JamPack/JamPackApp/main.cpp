70a47c1d5a0f45d8fdf3b0a2f34ed12c032a6ccc
Ahmed Zaman <ahmedrzaman@gmail.com>
2014-12-11 07:44:41 +0000

    Cleaned up main.cpp

--- src/JamPack/JamPackApp/main.cpp
+++ src/JamPack/JamPackApp/main.cpp
@@ -1,31 +1,12 @@
 #include "pch.h"
+
 #include "ResourcePacker.h"
-
-
-class Thing
-{
-};
-
-Thing* pThing = NULL;
-
-void setThing(Thing& n)
-{
-	pThing = &n;
-}
-
-void createThing()
-{
-	Thing n;
-	setThing(n);
-}
+#include "ResourcePackStream.h"
 
 void main()
 {
-	createThing();
-	int i = 0;
-	i++;
-
+	// Pack all the resources into 1 file.
 	Jampack::ResourcePacker rp;
 
-	// Pack all the resources into 1 file.
+	Jampack::ResourcePackStream rps;
 }
